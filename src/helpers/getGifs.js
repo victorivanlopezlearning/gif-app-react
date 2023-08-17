55a94343e090229f8427792a1d15c957010f52eb
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-08-17 14:41:18 -0600

    Change limit endpoint

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 export const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=ybQVEsQXvxrGKMX3CYoBIUtiFFndiirk&q=${category}&limit=10`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=ybQVEsQXvxrGKMX3CYoBIUtiFFndiirk&q=${category}&limit=12`;
   const response = await fetch(url);
   const { data } = await response.json();
 

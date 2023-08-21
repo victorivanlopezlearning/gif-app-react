d85f1a71595fe2a0e3d63ab6a38144148550b7da
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-08-21 13:21:21 -0600

    Add: prop types

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 
 export const GifItem = ({ title, url }) => {
 
@@ -10,3 +11,8 @@ export const GifItem = ({ title, url }) => {
     </div>
   )
 }
+
+GifItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
